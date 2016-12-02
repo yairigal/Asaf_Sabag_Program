b7d37458e54f97d7a321784892885132f43ac3fc
elroi <elroi19977@gmail.com>
2016-12-02 13:08:13 +0200

    starting the IO interface

---
+++ I_O/I_O_Interface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace I_O
+{
+    interface I_O_Interface
+    {
+        FileStream open_file(string path);
+    }
+}
--- Normalization/normalizer.cs
+++ Normalization/normalizer.cs
@@ -15,8 +15,5 @@ namespace Normalization
         {
 
         }
-
-
-        public
     }
 }
