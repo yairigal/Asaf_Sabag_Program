cc317366f1d28eb26258878afa02d03d6b893b23
elroi19977 <elroi19977@gmail.com>
2016-12-26 19:41:01 +0200

    trying to fix some errors

--- Normalization/app.cs
+++ Normalization/app.cs
@@ -8,16 +8,6 @@ namespace Normalization
 {
     class app
     {
-        public static void Main()
-        {
-            normalizer norm = new normalizer("C:\\Users\\user\\Desktop\\test\\alt.atheism", "");
-            IDictionary<NormaliztionMethods, bool> flags = new Dictionary<NormaliztionMethods, bool> ();
-            flags.Add(NormaliztionMethods.All_Capitals, true);
-            flags.Add(NormaliztionMethods.All_Lowercase, false);
-            flags.Add(NormaliztionMethods.No_HTML_Tags, false);
-            flags.Add(NormaliztionMethods.No_Punctuation, false);
-
-            norm.Normalize(flags);
-        }
+        
     }
 }
